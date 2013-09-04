2b21f0cd0ac693815b8aa5456a9c26cdbad5e324
Julian Gruber <julian@juliangruber.com>
2013-09-04 12:23:59 +0200

    example: add streams1 stream

--- example.js
+++ example.js
@@ -3,7 +3,16 @@ var co = require('co');
 var read = require('./');
 
 co(function*() {
+  console.log('streams2 stream\n');
   var res = yield request('http://google.com/');
+  var buf;
+  while(buf = yield read(res)) {
+    console.log(buf.toString());
+  }
+
+  console.log('streams1 stream\n');
+  var res = yield request('http://google.com/');
+  res.pause(); // force streams1 stream
   var buf;
   while(buf = yield read(res)) {
     console.log(buf.toString());
