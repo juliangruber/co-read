597b378af747fa66893cdd893e96b838e6defa8a
Julian Gruber <julian@juliangruber.com>
2014-07-13 15:23:48 +0200

    add debug

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
 /**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('co-read');
+
+/**
  * Expose `read`.
  */
 
@@ -29,25 +35,35 @@ function read1(stream) {
   stream.pause();
 
   return function(done) {
-    if (!stream.readable) return done();
+    if (!stream.readable) {
+      debug('not readable, all done');
+      return done();
+    }
 
+    debug('waiting for data, error or end');
     stream.on('data', ondata);
     stream.on('error', onerror);
     stream.on('end', onend);
+
+    debug('resuming');
     stream.resume();
 
     function ondata(data) {
+      debug('read %sb', data.length);
+      debug('pausing');
       stream.pause();
       cleanup();
       done(null, data);
     }
 
     function onerror(err) {
+      debug('error %s', err);
       cleanup();
       done(err);
     }
 
     function onend(data) {
+      debug('ended');
       cleanup();
       done(null, data);
     }
@@ -70,25 +86,30 @@ function read1(stream) {
 function read2(stream) {
   return function(done) {
     if (!stream.readable) {
+      debug('not readable, all done');
       return done();
     }
 
     function onreadable() {
+      debug('readable');
       cleanup();
       check();
     }
 
     function onend() {
+      debug('ended');
       cleanup();
       done(null, null);
     }
 
     function onerror(err) {
+      debug('got error %j', err);
       cleanup();
       done(err);
     }
 
     function listen() {
+      debug('waiting for readable, end or error');
       stream.on('readable', onreadable);
       stream.on('end', onend);
       stream.on('error', onerror);
@@ -103,8 +124,10 @@ function read2(stream) {
     function check() {
       var buf = stream.read();
       if (buf) {
+        debug('read %sb', buf.length);
         done(null, buf);
       } else {
+        debug('read -');
         listen();
       }
     }
